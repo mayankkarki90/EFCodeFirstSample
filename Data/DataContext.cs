1fa7c8464a7a2be51571045bb060f5526af503b4
Mayank Karki <mayank.karki90@hotmail.com>
2024-12-07 00:23:45 +0530

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DataContracts.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<VideoGame> VideoGames { get; set; }
+    }
+}
---
+++ DataContracts/Models/VideoGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContracts.Models
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+    }
+}
